a6c5005a73dc6f5c9c8b8e3654d7d8fa314b1b43
Abed001 <abed.alroufe@gmail.com>
2023-12-25 22:26:07 +0200

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Squash as Hamburger } from 'hamburger-react'
+import { useState } from 'react'
+
+export default function Navbar() {
+    const [open, setOpen] = useState(false)
+
+    const [iconState, setIconState] = useState("hamburger")
+
+    const handleMenuToggle = () => {
+        setOpen(!open);
+        if (iconState === 'hamburger') {
+            setIconState('close');
+        } else {
+            setIconState('hamburger');
+        }
+    };
+
+
+    return (
+
+        <div className='relative p-4 flex justify-between items-center  max-w-[1200px] lg:py-[1rem]  mx-auto border-b-2 '>
+            {/*light box when the hamburger menu open*/}
+            <div className={`fixed top-0 left-0 w-full h-screen bg-black opacity-75 transition-opacity duration-300 ease-in-out ${open ? '' : 'hidden'}`} />
+
+            <button className=' absolute  left-0 top-0 mt-7 md:hidden lg:hidden' onClick={handleMenuToggle}>
+                <Hamburger toggled={open} size={20} toggle={setOpen} />
+            </button>
+            <div className=' flex flex-row flex-1 w-full gap-x-10 items-center'>
+                <div><p className='mb-1 text-black font-semibold font-Kumbh text-[40px] tracking-tighter pl-10 lg-pl-0 '>sneakers</p></div>
+                <ul className='hidden cursor-pointer text-gray-500  items-center gap-x-5 lg:flex lg:flex-row lg:min-w-[500px] '>
+                    <li>Collections</li>
+                    <li>Men</li>
+                    <li>Women</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            
+            <div className=' absolute flex  top-0 left-0 p-30 min-w-[350px]'>
+                {open && (
+                    <div className='bg-white w-full  p-5 '>
+                        <button className='pl-5 absolute  left-0 top-0 mt-7 md:hidden lg:hidden' onClick={handleMenuToggle}>
+                            <Hamburger toggled={open} size={20} toggle={setOpen} /> </button>
+                        <ul className=' ml-4 mt-[80px] cursor-pointer text-black h-screen min-w-[300px] font-bold'>
+                            <li className='mb-5'>Collections</li>
+                            <li className='mb-5'>Men</li>
+                            <li className='mb-5'>Women</li>
+                            <li className='mb-5'>About</li>
+                            <li className='mb-5'>Contact</li>
+                        </ul></div>
+
+                )}</div>
+
+
+            <div className=' flex gap-x-10 justify-evenly items-center'>
+                <svg className='cursor-pointer' width="22" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#69707D" fill-rule="nonzero" />
+                </svg>
+                <img className='w-10 h-10 cursor-pointer' src="./images/imageavatar.png" alt="person avatar" />
+            </div>
+
+
+        </div>
+
+
+    )
+}
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,184 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import { IoCartOutline } from "react-icons/io5";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoCloseSharp } from "react-icons/io5";
+import { useState, useEffect } from 'react'
+
+export default function Home() {
+
+  const [currentIndex, setcurrentIndex] = useState(0)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1
+    setcurrentIndex(newIndex)
+  }
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setcurrentIndex(newIndex)
+  }
+
+  const goToSlide = (slideIndex) => {
+    setcurrentIndex(slideIndex)
+  }
+
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'ArrowLeft') {
+      prevSlide();
+    } else if (event.key === 'ArrowRight') {
+      nextSlide();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [currentIndex]);
+
+  const slides = [
+    {
+      url: '/images/image-product-1.jpg',
+    },
+    {
+      url: './images/image-product-2.jpg',
+    },
+    {
+      url: './images/image-product-3.jpg',
+    },
+
+    {
+      url: './images/image-product-4.jpg',
+    },
+
+
+  ];
+
+
+  return (
+    <> <Navbar />
+      <div className=' font-bold font-Kumbh flex flex-col justify-center items-start lg:p-20  '>
+        <div className=' flex flex-col justify-between items-center gap-x-20 lg:flex-row '>
+
+          {/*product image and thumbnail on large screen*/}
+          <div className='sm lg:flex lg:flex-col'>
+
+            <div onClick={openModal} style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className=' max-w-[200px] h-[500px] bg-cover bg-center bg-no-repeat duration-500 lg:min-w-[400px] lg:max-h-[400px] lg:rounded-xl lg:bg-top'></div>
+
+            <div className='lg:flex lg:flex-row lg:w-full lg:justify-evenly'>
+              {slides.map((slide, slideIndex) => (
+                <div className={`hover:opacity-[50%] mt-10 gap-x-10 rounded-lg w-[80px] h-[80px] bg-cover bg-center bg-no-repeat duration-500  ${slideIndex == currentIndex ? 'border-4 border-orange-500 opacity-[50%]' : ''}`} key={slideIndex} onClick={() => goToSlide(slideIndex)} style={{ backgroundImage: `url(${slide.url})` }}></div>
+              ))}
+
+            </div>
+          </div>
+          {/*product image  on small screen*/}
+          <div style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className='lg:hidden w-full h-[500px] bg-cover bg-center bg-no-repeat duration-500  '></div>
+
+
+          {/*when clicking on large image the modals open*/}
+          {isModalOpen && (
+            <div className="sm fixed inset-0 flex items-center justify-center">
+              <div className="absolute inset-0 bg-black opacity-80" onClick={closeModal}></div>
+              <div className="relative z-10 p-4">
+
+                <div><IoCloseSharp size={30} className='mb-5 text-orange-500 ml-auto cursor-pointer ' onClick={closeModal} /></div>
+
+                {/*product image and thumbnail on large screen*/}
+                <div className='sm lg:flex lg:flex-col'>
+
+                  <div onClick={openModal} style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className=' max-w-[200px] h-[500px] bg-cover bg-center bg-no-repeat duration-500 lg:min-w-[400px] lg:max-h-[400px] lg:rounded-xl lg:bg-top'></div>
+
+                  <div className='lg:flex lg:flex-row lg:w-full lg:justify-evenly'>
+                    {slides.map((slide, slideIndex) => (
+                      <div className={`hover:opacity-[50%] mt-10 gap-x-10 rounded-lg w-[80px] h-[80px] bg-cover bg-center bg-no-repeat duration-500  ${slideIndex == currentIndex ? 'border-4 border-orange-500 opacity-[50%]' : ''}`} key={slideIndex} onClick={() => goToSlide(slideIndex)} style={{ backgroundImage: `url(${slide.url})` }}></div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+
+              {/*slider arrow for desktop light box*/}
+
+              {/*slider left right*/}
+              <div className='z-50 shadow absolute  top-[250px] left-[449px]  bg-white flex flex-row  items-center justify-around w-12 h-12  rounded-full '>
+                <IoIosArrowBack className='cursor-pointer  hover:text-orange-500' size={20} onClick={nextSlide} />
+
+              </div>
+
+              <div className='z-50 shadow absolute  top-[250px] right-[449px]  bg-white flex flex-row  items-center justify-around w-12 h-12  rounded-full '>
+                <IoIosArrowForward className='cursor-pointer  hover:text-orange-500' size={20} onClick={prevSlide} />
+              </div>
+            </div>
+
+
+          )}
+
+
+          <div className='p-4 mt-2'>
+            <p className='uppercase text-paleorange text-md mb-5  text-orange-500'>sneaker company</p>
+            <h1 className='uppercase block text-black text-3xl'>fall limited edition</h1>
+            <h1 className='uppercase block text-black text-3xl mb-5'>sneaker</h1>
+            <p className='text-left text-xl mb-5 text-gray-500 font-normal'>
+              These low-profile sneakers are your perfect casual wear companion. Featuring a
+              durable rubber outer sole, they’ll withstand everything the weather can offer.
+            </p>
+
+            <div className='w-full flex flex-row justify-between items-center mb-5 lg:flex-col  '>
+              <div className='lg:w-full lg:justify-start lg:flex lg:flex-col'>
+                <div className='w-full flex flex-row gap-x-5 items-center justify-center lg:justify-start'>
+                  <span className=' text-xl text-black'>$125.00</span>
+                  <span className=' bg-orange-100 text-orange-500 rounded-md px-2 py-1'> 50%</span>
+                </div>
+                <span className=' text-gray-400 line-through'> $250.00</span>
+              </div>
+            </div>
+
+            <div className='flex flex-col lg:flex-row lg:gap-x-5 '>
+              <div className='mb-5 flex justify-around items-center rounded-lg flex-row bg-gray-100 py-2 gap-x-20 lg:max-w-[300px]'>
+                <button className='text-orange-500 text-4xl lg:text-1xl lg:ml-2'>-</button>
+                <span className='text-black text-2xl lg:text-lg'>0</span>
+                <button className='text-orange-500 text-4xl lg:text-1xl lg:mr-2'>+</button></div>
+
+              <button className='mb-5 rounded-lg gap-x-3 bg-orange-500 p-4 text-white flex flex-row justify-center items-center lg:flex-1 lg:max-w-[400px] '>
+                <IoCartOutline />
+                <span>Add to cart</span></button>
+            </div>
+          </div>
+        </div>
+
+        {/*slider arrow for mobile*/}
+        <div className='lg:hidden flex flex-row justify-between w-full'>
+          {/*slider left right*/}
+          <div className='shadow absolute  top-[312px] left-[400px]  bg-white flex flex-row  items-center justify-around w-12 h-12  rounded-full '>
+            <IoIosArrowForward className='cursor-pointer' size={20} onClick={nextSlide} />
+
+          </div>
+
+          <div className=' lg:hidden shadow absolute  top-[312px] right-[400px]  bg-white flex flex-row  items-center justify-around w-12 h-12  rounded-full '>
+
+            <IoIosArrowBack className='cursor-pointer' size={20} onClick={prevSlide} />
+          </div>
+        </div>
+
+
+
+
+      </div>
+    </>
+  )
+}
